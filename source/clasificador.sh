e3a91b5d1dc1b8c3317a69607d055401f83eef74
Fran <fran.dorr@gmail.com>
2014-11-01 16:41:52 -0300

    Cambios en clasificador

--- source/clasificador.sh
+++ source/clasificador.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 arch=$1    # archivo a modificar
-SMILExtract -C config/IS10_paraling.conf -I $arch -O out.arff -instname "?"
-sed -i 's/@attribute name string/@attribute sexo {m,f}/g' out.arff
-java -cp /usr/share/java/weka.jar weka.filters.unsupervised.attribute.Remove -V -R 1, 685, 684, 676, 1441, 1432, 1442, 678, 686, 1440, 691, 713, 712, 1446, 1434, 1522, 256, 266, 265, 264, 707, 327, 1437, 258, 271, 319, 1436, 277, 1330, 321, 328, 329, 279, 285, 270, 286 -i inp.arff -o out.arff
-java -cp /usr/share/java/weka.jar weka.classifiers.lazy.IB1 -l lazyIB1.model -T inp.arff
+rm out.arff
+rm inp.arff
+SMILExtract -C /opensmile/config/IS10_paraling.conf -I $arch -O out.arff -instname m -classes {m,f} -classlabel m
+java -cp /usr/share/java/weka.jar weka.filters.unsupervised.attribute.Remove -V -R 1,685,684,676,1441,1432,1442,678,686,1440,691,713,712,1446,1434,1522,256,266,265,264,707,327,1437,258,271,319,1436,277,1330,321,328,329,279,285,270,286 -i out.arff -o inp.arff
+sed -i 's/@attribute name string/@attribute sexo {m,f}/g' inp.arff
+java -cp /usr/share/java/weka.jar weka.classifiers.lazy.IB1 -l lazyIB1.model -T inp.arff -p 0
