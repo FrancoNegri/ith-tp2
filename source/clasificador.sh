810cb799607e2a9aafe2a05221873733fbf6a0bf
Fran <fran.dorr@gmail.com>
2014-11-02 12:48:21 -0300

    mod clasificador

--- source/clasificador.sh
+++ source/clasificador.sh
@@ -3,7 +3,7 @@ arch="$1"    # archivo a modificar
 rm processing/log >> processing/log
 rm processing/out.arff >> processing/log
 rm processing/inp.arff >> processing/log
-/home/franco/Repos/ith-tp2/opensmile-2.0-rc1/opensmile/SMILExtract -C Configs/IS10_paraling.conf -I "$arch" -O processing/out.arff -instname replaceHere -noconsoleoutput 1 -logfile processing/log -appendLogfile 1 >> processing/log
+SMILExtract -C Configs/IS10_paraling.conf -I "$arch" -O processing/out.arff -instname replaceHere -noconsoleoutput 1 -logfile processing/log -appendLogfile 1 >> processing/log
 java -cp /usr/share/java/weka.jar weka.filters.unsupervised.attribute.Remove -V -R 1,685,684,676,1441,1432,1442,678,686,1440,691,713,712,1446,1434,1522,256,266,265,264,707,327,1437,258,271,319,1436,277,1330,321,328,329,279,285,270,286 -i processing/out.arff -o processing/inp.arff >> processing/log
 sed -i 's/@attribute name string/@attribute sexo {m,f}/g' processing/inp.arff
 sed -i 's/replaceHere/?/g' processing/inp.arff
